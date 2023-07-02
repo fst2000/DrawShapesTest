a9f8c4703f26c85a9a9eff458f3bd5016d0fc2ed
fst2000 <fst.18.work@gmail.com>
2023-07-02 20:05:28 +0400

    Chess rotation

--- ChessDesk.java
+++ ChessDesk.java
@@ -1,30 +1,46 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Timer;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ChessDesk extends JPanel
 {
     JFrame frame;
-    public ChessDesk(JFrame frame) {
+    int size;
+    long startTime = System.currentTimeMillis();
+    public ChessDesk(JFrame frame, int size) {
         this.frame = frame;
+        this.size = size;
     }
 
     @Override
     public void paintComponent(Graphics g)
     {
+        Graphics2D g2d = (Graphics2D) g;
+        double passedTime = (double)(System.currentTimeMillis() - startTime);
         int length = 8;
-        int width = frame.getWidth() / length;
-        int height = frame.getHeight() / length;
-
-        for(int i = 0; i <= length; i++)
+        int width = frame.getWidth();
+        int height = frame.getHeight();
+        int rectSize = size / length;
+        g2d.rotate(passedTime / 1000, width >> 1, height >> 1);
+        for(int x = 0; x < length; x++)
         {
-            for(int j = 0; j <= length; j++)
+            for(int y = 0; y < length; y++)
             {
-                if((i + j) % 2 == 0) g.setColor(Color.WHITE);
-                else g.setColor(Color.BLACK);
-                g.fillRect(i * width, j * height, width, height);
+                if((x + y) % 2 == 0) g2d.setColor(Color.GRAY);
+                else g2d.setColor(Color.DARK_GRAY);
+                g2d.fillRect(
+                    ((width - size) >> 1) + x * rectSize,
+                    ((height - size) >> 1) + y * rectSize,
+                    rectSize,
+                    rectSize);
             }
         }
+        frame.repaint();
     }
 }
--- Program.java
+++ Program.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Program
 {
@@ -11,10 +12,9 @@ public class Program
         {
             JFrame frame = new JFrame("Rectangle");
             frame.setBackground(Color.BLACK);
-            frame.setSize(500, 500);
-            ChessDesk chess = new ChessDesk(frame);
+            frame.setSize(300, 300);
+            ChessDesk chess = new ChessDesk(frame, 100);
             frame.add(chess);
-
             frame.setVisible(true);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         });
